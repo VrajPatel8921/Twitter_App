90401dd3a5b6bcf5aa4172dd3b5977315a4eff52
Vraj <vrajp8921@gmail.com>
2025-06-16 18:10:45 +0530

    Database Config

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,7 @@ import './App.css'
 import conf from './conf/conf'
 
 function App() {
-  console.log(conf.appwriteUrl)
-  console.log(conf.appwriteProjectId)
-  console.log(conf.appwriteDatabaseId)
-  console.log(conf.appwriteCollectionId)
-  console.log(conf.appwriteBucketId)
+  
   return (
     <>
       <h1> A Twitter App With AppWrite</h1>
