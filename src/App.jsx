0e80c1e66a0247500cbe37e522374ac0ad4297d8
Vraj <vrajp8921@gmail.com>
2025-06-17 19:01:01 +0530

    Setup Redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
 
+import { useEffect, useState } from 'react'
 import './App.css'
 import conf from './conf/conf'
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth';
+import {login,logout} from "./store/authSlice";
+import { Footer, Header } from './components';
+import { Outlet } from 'react-router-dom';
 
 function App() {
-  
-  return (
-    <>
-      <h1> A Twitter App With AppWrite</h1>
-    </>
-  )
+    const [loading,setLoading]=useState(true);
+    const dispatch=useDispatch();
+
+    useEffect(()=>{
+      authService.getCurrentUser()
+      .then((userData)=>{
+        if(userData){
+          dispatch(login({userData}))
+        }else{
+          dispatch(logout());
+        }
+      })
+      .finally(()=> setLoading(false));
+    },[])
+
+  return !loading ? (
+    <div className='min-h-screen min-w-screen flex flex-wrap content content-between bg-gray-400'>
+      <div className='w-full block'>
+          <Header/>
+          <main>
+            Todo:{/* <Outlet/> */}
+          </main>
+          <Footer/>
+      </div>
+    </div>
+  ):null
 }
 
 export default App
